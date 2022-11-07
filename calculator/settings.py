2aa8011c9f8f5eb1d7399866f60a22c9a0fd2bbf
Jeffrey Whewhetu <jeffwhewhetu@gmail.com>
2022-11-07 13:04:22 +0100

    update code

--- calculator/settings.py
+++ calculator/settings.py
@@ -125,8 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "static"
 
 django_heroku.settings(locals())
 # Default primary key field type
